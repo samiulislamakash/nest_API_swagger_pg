47506f27fa7bef90bc32ec478c3f985892461555
Samiul Islam <samiulislamw.w.w@gmail.com>
2021-10-29 22:18:08 +0600

    base contorller is depricated for dto :)

--- src/@modules/product/product.controller.ts
+++ src/@modules/product/product.controller.ts
@@ -8,20 +8,58 @@ import {
   Put,
   Param,
   Delete,
+  Query,
+  Patch,
 } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { BaseController } from 'src/@base/base.controller';
+import { Filter } from 'src/@base/dto/filter.dto';
 
 @ApiTags('Product')
 @Controller('product')
-export class ProductController extends BaseController<
-  CreateProductDto,
-  UpdateProductDto,
-  BulkUpdateProductDto
-> {
-  constructor(private readonly productService: ProductService) {
-    super(productService, ['']);
+export class ProductController {
+  modelRelations: string[] = [''];
+
+  constructor(private readonly productService: ProductService) {}
+
+  @Post()
+  create(@Body() payload: CreateProductDto): Promise<any> {
+    return this.productService._create(payload, this.modelRelations);
+  }
+
+  @Post('bulkCreate')
+  bulkCreate(@Body() payload: CreateProductDto[]): Promise<any> {
+    return this.productService._bulkCreate(payload, this.modelRelations);
+  }
+
+  @Patch('bulkUpdate')
+  bulkUpdate(@Body() payload: BulkUpdateProductDto): Promise<any> {
+    return this.productService._bulkUpdate(payload, this.modelRelations);
+  }
+
+  @Delete('bulkDelete')
+  bulkDelete(@Body() payload: string[]): Promise<any> {
+    return this.productService._bulkDelete(payload);
+  }
+
+  @Get()
+  filter(@Query() query: Filter): Promise<any> {
+    return this.productService._filter(query, this.modelRelations);
+  }
+
+  @Get(':id')
+  getById(@Param() id: string): Promise<any> {
+    return this.productService._getById(id, this.modelRelations);
+  }
+
+  @Patch(':id')
+  update(@Param() id: string, @Body() payload: UpdateProductDto): Promise<any> {
+    return this.productService._update(id, payload, this.modelRelations);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): Promise<any> {
+    return this.productService._remove(id);
   }
 }
